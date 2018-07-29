39fa858670acc241f19775be731fe8f8ca0bb20b
Sergey Popov <poserg@gmail.com>
2018-07-29 21:32:27 +0300

    Add main functions

---
+++ fns/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.0.1'
+
+from fns.app import check_receipt, revise_receipt
+
+class Receipt:
+    def __init__(self, fn, fp, fd, purchase_date=None, total=None):
+        self.fn = fn
+        self.fp = fp
+        self.fd = fd
+        self.purchase_date = purchase_date
+        self.total = total
+
+
